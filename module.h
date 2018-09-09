4c24b325e099fab7d39671452cf37b1a6103a7e5
Jack Clarke <jackojc@gmail.com>
2018-09-09 19:37:00 +0100

    Dim colours, rainbow per char and code cleanup.
    - Added dim colours.
    
    - Added some utility functions for indentation and formatting.
    
    - minor cleanup.
    
    - added "normal" colours to reset to default term colours.
    
    - rainbow functions now operate per character.
    
    - updated readme.

--- module.h
+++ module.h
@@ -10,12 +10,43 @@
 #include <array>
 #include <utility>
 #include <random>
+#include <sstream>
 
 
 #include "rang/include/rang.hpp"
 
 
 namespace ws::module {
+    // Macros
+    #define NEW_COLOUR(name, val) constexpr auto name = val;
+
+
+
+    // Utility functions
+    std::string repeat(int n, char c) {
+        return std::string(n, c);
+    }
+
+
+    std::string spaces(int n) {
+        return ws::module::repeat(n, ' ');
+    }
+
+
+    std::string tabs(int n) {
+        return ws::module::repeat(n, '\t');
+    }
+
+
+    constexpr auto indent = ws::module::tabs;
+
+
+    std::string lines(int n) {
+        return ws::module::repeat(n, '\n');
+    }
+
+
+
     // Receive data in chunks.
     // Callback = (const std::string& buffer, int chunk_id, bool eof);
     inline void receive(
@@ -61,9 +92,6 @@ namespace ws::module {
 
 
     // Colours.
-    #define NEW_COLOUR(name, val) constexpr auto name = val;
-
-
     namespace colour {
         namespace fg {
             NEW_COLOUR(black,   rang::fg::black);
@@ -74,6 +102,7 @@ namespace ws::module {
             NEW_COLOUR(magenta, rang::fg::magenta);
             NEW_COLOUR(cyan,    rang::fg::cyan);
             NEW_COLOUR(grey,    rang::fg::gray);
+            NEW_COLOUR(normal,  rang::fg::reset);
 
             namespace bright {
                 NEW_COLOUR(black,   rang::fgB::black);
@@ -84,6 +113,7 @@ namespace ws::module {
                 NEW_COLOUR(magenta, rang::fgB::magenta);
                 NEW_COLOUR(cyan,    rang::fgB::cyan);
                 NEW_COLOUR(grey,    rang::fgB::gray);
+                NEW_COLOUR(normal,  rang::fg::reset);
             }
         }
 
@@ -97,6 +127,7 @@ namespace ws::module {
             NEW_COLOUR(magenta, rang::bg::magenta);
             NEW_COLOUR(cyan,    rang::bg::cyan);
             NEW_COLOUR(grey,    rang::bg::gray);
+            NEW_COLOUR(normal,  rang::bg::reset);
 
             namespace bright {
                 NEW_COLOUR(black,   rang::bgB::black);
@@ -107,6 +138,7 @@ namespace ws::module {
                 NEW_COLOUR(magenta, rang::bgB::magenta);
                 NEW_COLOUR(cyan,    rang::bgB::cyan);
                 NEW_COLOUR(grey,    rang::bgB::gray);
+                NEW_COLOUR(normal,  rang::bg::reset);
             }
         }
 
@@ -115,9 +147,6 @@ namespace ws::module {
         NEW_COLOUR(warn,   fg::bright::yellow);
         NEW_COLOUR(error,  fg::bright::red);
     }
-
-
-    #undef NEW_COLOUR
 
 
 
@@ -137,6 +166,15 @@ namespace ws::module {
         // Reset style, BG and FG all at once.
         struct Reset {};
 
+        // Dim colour style.
+        struct DimFG {
+            decltype(colour::fg::black) colour;
+        };
+
+        struct DimBG {
+            decltype(colour::bg::black) colour;
+        };
+
         // Notice style, set bold and notice colour.
         struct Notice {};
 
@@ -149,29 +187,80 @@ namespace ws::module {
 
 
 
+    namespace colour {
+        namespace fg::dim {
+            NEW_COLOUR(black,   details::DimFG{colour::fg::black});
+            NEW_COLOUR(red,     details::DimFG{colour::fg::red});
+            NEW_COLOUR(green,   details::DimFG{colour::fg::green});
+            NEW_COLOUR(yellow,  details::DimFG{colour::fg::yellow});
+            NEW_COLOUR(blue,    details::DimFG{colour::fg::blue});
+            NEW_COLOUR(magenta, details::DimFG{colour::fg::magenta});
+            NEW_COLOUR(cyan,    details::DimFG{colour::fg::cyan});
+            NEW_COLOUR(grey,    details::DimFG{colour::fg::grey});
+            NEW_COLOUR(normal,  rang::style::reset);
+        }
+
+        namespace bg::dim {
+            NEW_COLOUR(black,   details::DimBG{colour::bg::black});
+            NEW_COLOUR(red,     details::DimBG{colour::bg::red});
+            NEW_COLOUR(green,   details::DimBG{colour::bg::green});
+            NEW_COLOUR(yellow,  details::DimBG{colour::bg::yellow});
+            NEW_COLOUR(blue,    details::DimBG{colour::bg::blue});
+            NEW_COLOUR(magenta, details::DimBG{colour::bg::magenta});
+            NEW_COLOUR(cyan,    details::DimBG{colour::bg::cyan});
+            NEW_COLOUR(grey,    details::DimBG{colour::bg::grey});
+            NEW_COLOUR(normal,  rang::style::reset);
+        }
+    }
+
+
+
     // Overloads.
-    inline std::ostream& operator<<(std::ostream& os, const Reset& obj) {
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::Reset& obj
+    ) {
         return (os << rang::style::reset << rang::fg::reset << rang::bg::reset);
     }
 
 
 
-    inline std::ostream& operator<<(std::ostream& os, const Notice& obj) {
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::DimFG& obj
+    ) {
+        return (os << rang::style::dim << obj.colour);
+    }
+
+
+
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::DimBG& obj
+    ) {
+        return (os << rang::style::dim << obj.colour);
+    }
+
+
+
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::Notice& obj
+    ) {
         return (os << style::bold << colour::notice);
     }
 
 
 
-    inline std::ostream& operator<<(std::ostream& os, const Warn& obj) {
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::Warn& obj
+    ) {
         return (os << style::bold << colour::warn);
     }
 
 
 
-    inline std::ostream& operator<<(std::ostream& os, const Error& obj) {
+    inline std::ostream& operator<<(
+        std::ostream& os, const details::Error& obj
+    ) {
         return (os << style::bold << colour::error);
     }
-
 
 
 
@@ -209,7 +298,7 @@ namespace ws::module {
 
 
     // Logging and IO.
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& print(Ts&&... args) {
         return (
             ws::module::printer <<
@@ -221,7 +310,7 @@ namespace ws::module {
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& pipe(Ts&&... args) {
         return (ws::module::piper << ... << std::forward<Ts&&>(args));
     }
@@ -232,14 +321,14 @@ namespace ws::module {
 
 
     // Logging and IO...
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& println(Ts&&... args) {
         return ws::module::print(std::forward<Ts&&>(args)...) << '\n';
     }
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& pipeln(Ts&&... args) {
         return ws::module::pipe(std::forward<Ts&&>(args)...) << '\n';
     }
@@ -250,7 +339,7 @@ namespace ws::module {
 
 
     // Logging with colour.
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& notice(Ts&&... args) {
         ws::module::print(style::notice, details::symbol::notice);
         return ws::module::print(std::forward<Ts&&>(args)...);
@@ -258,7 +347,7 @@ namespace ws::module {
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& warn(Ts&&... args) {
         ws::module::print(style::warn, details::symbol::warn);
         return ws::module::print(std::forward<Ts&&>(args)...);
@@ -266,7 +355,7 @@ namespace ws::module {
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& error(Ts&&... args) {
         ws::module::print(style::error, details::symbol::error);
         return ws::module::print(std::forward<Ts&&>(args)...);
@@ -275,21 +364,21 @@ namespace ws::module {
 
 
     // Print lines too.
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& noticeln(Ts&&... args) {
         return ws::module::notice(std::forward<Ts&&>(args)..., '\n');
     }
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& warnln(Ts&&... args) {
         return ws::module::warn(std::forward<Ts&&>(args)..., '\n');
     }
 
 
 
-    template<typename... Ts>
+    template <typename... Ts>
     inline std::ostream& errorln(Ts&&... args) {
         return ws::module::error(std::forward<Ts&&>(args)..., '\n');
     }
@@ -329,18 +418,23 @@ namespace ws::module {
 
 
 
+    template <typename T>
+    inline std::ostream& rainbow(T&& arg) {
+        std::stringstream ss;
+        ss << arg;
 
-
-    template<typename... Ts>
-    inline std::ostream& rainbow(Ts&&... args) {
-        auto _ = { (
-            ws::module::print(
-                details::random_colour(),
-                std::forward<Ts>(args)
-            ), 0
-        )... };
+        for (const auto& chr: ss.str())
+            ws::module::print(details::random_colour(), chr);
 
         return ws::module::printer;
+    }
+
+
+
+    template <typename T1, typename T2, typename... Ts>
+    inline std::ostream& rainbow(T1 arg1, T2 arg2, Ts&&... args) {
+        ws::module::rainbow(arg1);
+        return ws::module::rainbow(arg2, std::forward<Ts&&>(args)...);
     }
 
 
@@ -349,6 +443,11 @@ namespace ws::module {
     inline std::ostream& rainbowln(Ts&&... args) {
         return ws::module::rainbow(std::forward<Ts&&>(args)..., '\n');
     }
+
+
+
+    // Remove all macros.
+    #undef NEW_COLOUR
 }
 
 
