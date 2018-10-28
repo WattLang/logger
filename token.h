bfc4fcfa391c3955e8d6cfdd313065c85c2010ec
Jack Clarke <jackojc@gmail.com>
2018-10-28 17:00:14 +0000

    Fix implicit cast warnings and readabilty improvements (#25)
    * Revert "Merge remote-tracking branch 'origin/master'"
    
    This reverts commit 43ed4f7eb1c2505b6cfc6acc3f46442a82f81b92.
    
    * Fix implicit cast warnings and some readability improvements

--- token.h
+++ token.h
@@ -1,12 +1,12 @@
 #pragma once
 
+
 #include <cstdint>
 #include <iostream>
 #include <string>
 
+
 namespace ws::token {
-
-
     using type_t = std::uint16_t;
 
 
@@ -17,6 +17,9 @@ namespace ws::token {
         std::string content;
     };
 
+
+
+
     /*
         8 bytes: content's size
         <size> bytes: content
@@ -24,37 +27,58 @@ namespace ws::token {
         8 bytes: line
         8 bytes: column
     */
+
     inline std::ostream& serialize(std::ostream& os, Token const& token) {
-        os.write(reinterpret_cast<const char*>(&token),               offsetof(Token, content));
-        std::size_t size = token.content.size();
-        os.write(reinterpret_cast<const char*>(&size               ), sizeof(size            ));
-        os.write(                               token.content.data(),        size             );
+        auto size = static_cast<std::streamsize>(token.content.size());
+
+
+        os.write(
+            reinterpret_cast<const char*>(&token), offsetof(Token, content)
+        );
+
+        os.write(reinterpret_cast<const char*>(&size), sizeof(size));
+        os.write(token.content.data(), size);
+
+
         return os;
     }
 
+
+
+
     inline std::istream& deserialize(std::istream& is, Token& token) {
-        auto read = [] (std::istream& is, auto* prop, std::streamsize size) {
+
+        auto read = [] (
+            std::istream& is,
+            auto* prop,
+            std::streamsize size
+        ) {
             if (is.readsome(reinterpret_cast<char*>(prop), size) != size) {
                 is.setstate(std::ios::failbit);
                 return false;
             }
+
+
             return true;
         };
+
+
         is.clear(); // remove flags
+
 
         if (!read(is, &token, offsetof(Token, content)))
             return is;
+
 
         std::size_t size;
         if (!read(is, &size, sizeof(size)))
             return is;
 
+
         token.content.resize(size);
-        read(is, token.content.data(), size);
+        read(is, token.content.data(), static_cast<std::streamsize>(size));
+
 
         return is;
     }
-
-
-
 }
