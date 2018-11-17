b3525c1c8b7b489de0c2df3376f69a098787d821
Jack Clarke <jackojc@gmail.com>
2018-11-17 15:21:50 +0000

    TokenGroup (#40)
    * TokenGroup
    
    TokenGroup wraps a string and vector of tokens together so their lifetimes are bound.
    
    This means we can use string_views for everything leading to significant speedups.
    
    * Use alias and new constructor
    
    - Use vec_t alias for tokens member
    - New constructor so you dont have to specify a default size for tokens
    
    * Renaming
    
    - Rename TokenGroup to Group to resolve redundant naming.
    
    - Rename TokenType to Type

--- token.h
+++ token.h
@@ -3,20 +3,23 @@
 
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <utility>
 #include <string>
+#include <string_view>
 
 
 namespace ws::token {
-    using type_t = std::uint16_t;
+    using Type = std::uint16_t;
 
 
 
 
     struct Token {
-        type_t type;
+        Type type;
         std::uint32_t line;
         std::uint32_t column;
-        std::string content;
+        std::string_view content;
 
 
 
@@ -24,10 +27,10 @@ namespace ws::token {
 
 
         Token(
-            type_t type_,
+            Type type_,
             std::uint32_t line_,
             std::uint32_t column_,
-            const std::string& content_
+            std::string_view content_
         ):
             type(type_),
             line(line_),
@@ -40,14 +43,243 @@ namespace ws::token {
 
 
         Token(
-            type_t type_,
-            const std::string& content_
+            Type type_,
+            std::string_view content_
         ): type(type_), content(content_) {}
 
 
 
-        Token(type_t type_): type(type_) {}
+        Token(Type type_): type(type_) {}
     };
+
+
+
+
+
+
+
+
+
+
+
+    struct Group {
+        using vec_t = std::vector<Token>;
+
+        using size_type = vec_t::size_type;
+
+        using iterator = vec_t::iterator;
+        using const_iterator = vec_t::const_iterator;
+
+        using reverse_iterator = vec_t::reverse_iterator;
+        using const_reverse_iterator = vec_t::const_reverse_iterator;
+
+
+
+
+
+
+        std::string source;
+        vec_t tokens;
+
+
+        Group() = default;
+
+        Group(const std::string& str):
+            source(str)
+        {
+            tokens.reserve(100);
+        }
+
+        Group(const std::string& str, float ratio):
+            source(str)
+        {
+            auto buffer_size = str.size() / ratio;
+            tokens.reserve(buffer_size);
+        }
+
+
+
+
+
+        // Iterators
+        auto begin() const noexcept {
+            return tokens.begin();
+        }
+
+        auto end() const noexcept {
+            return tokens.end();
+        }
+
+        auto rbegin() const noexcept {
+            return tokens.rbegin();
+        }
+
+        auto rend() const noexcept {
+            return tokens.rend();
+        }
+
+
+
+
+
+
+
+
+
+
+        // Element access.
+        Token& at(size_type i) {
+            return tokens.at(i);
+        }
+
+        const Token& at(size_type i) const {
+            return tokens.at(i);
+        }
+
+
+        Token& operator[](size_type i) {
+            return tokens[i];
+        }
+
+        const Token& operator[](size_type i) const {
+            return tokens[i];
+        }
+
+
+        Token& front() {
+            return tokens.front();
+        }
+
+        const Token& front() const {
+            return tokens.front();
+        }
+
+
+
+        Token& back() {
+            return tokens.back();
+        }
+
+        const Token& back() const {
+            return tokens.back();
+        }
+
+
+
+        Token* data() noexcept {
+            return tokens.data();
+        }
+
+        const Token* data() const noexcept {
+            return tokens.data();
+        }
+
+
+
+
+
+
+
+
+
+        // Capacity.
+
+        bool empty() const noexcept {
+            return tokens.empty();
+        }
+
+
+
+        size_type size() const noexcept {
+            return tokens.size();
+        }
+
+
+
+
+        size_type max_size() const noexcept {
+            return tokens.max_size();
+        }
+
+
+
+        void reserve(size_type cap) {
+            tokens.reserve(cap);
+        }
+
+        size_type capacity() const noexcept {
+            return tokens.capacity();
+        }
+
+
+
+        void shrink_to_fit() {
+            tokens.shrink_to_fit();
+        }
+
+
+
+
+
+
+        // Modifiers.
+        void clear() noexcept {
+            tokens.clear();
+        }
+
+
+
+
+
+        iterator erase(iterator pos) {
+            return tokens.erase(pos);
+        }
+
+        iterator erase(const_iterator pos) {
+            return tokens.erase(pos);
+        }
+
+        iterator erase(iterator first, iterator last) {
+            return tokens.erase(first, last);
+        }
+
+        iterator erase(const_iterator first, const_iterator last) {
+            return tokens.erase(first, last);
+        }
+
+
+
+
+        template <typename... Ts>
+        const Token& emplace(Ts&&... args) {
+            return tokens.emplace_back(std::forward<Ts>(args)...);
+        }
+
+
+
+
+        void resize(size_type count, Token value = Token()) {
+            tokens.resize(count, value);
+        }
+
+        void resize(size_type count) {
+            tokens.resize(count);
+        }
+
+        void resize(size_type count, const Token& value) {
+            tokens.resize(count, value);
+        }
+
+    };
+
+
+
+
+
+
+
+
+
 
 
 
